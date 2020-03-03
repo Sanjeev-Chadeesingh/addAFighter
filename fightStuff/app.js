ae965fc6c4025bbf4703e94da4b0eee35d6d28ac
Leolal <leolal1984@icloud.com>
2020-03-03 15:30:35 -0500

    added fighter templates

---
+++ fightStuff/app.js
@@ -0,0 +1,16 @@
+var controllerOne = (function() {
+
+
+
+})();
+
+
+var controllerTwo = (function() {
+
+})();
+
+console.log('Hiyah There!');
+
+document.querySelector('.btn_submit').addEventListener('click', function(){
+    alert('fighter added to the list!');
+});
